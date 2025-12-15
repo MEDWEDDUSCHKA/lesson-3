ee36f73ceccd9ef897740e5781d143d3d3d27395
MEDWED <egor2239@gmail.com>
2025-12-15 12:54:47 +0300

    fixed comments

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -6,79 +6,84 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-   private static final String create = "CREATE TABLE IF NOT EXISTS users (" +
+   private static final String CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS users (" +
             "id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
             "name VARCHAR(50), " +
             "last_name VARCHAR(50), " +
             "age TINYINT" +
             ")";
-   private static final String drop = "DROP TABLE IF EXISTS users";
-   private static final String clean = "DELETE FROM users";
+   private static final String DROP_TABLE_SQL = "DROP TABLE IF EXISTS users";
+   private static final String CLEAN_TABLE_SQL = "DELETE FROM users";
 
-   private final SessionFactory sessionFactory;
-
-   public UserDaoHibernateImpl() {
-       Util util = new Util();
-       this.sessionFactory = util.getSessionFactory();
-   }
+   private final SessionFactory sessionFactory = new Util().getSessionFactory();
 
     @Override
     public void createUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createNativeQuery(create).executeUpdate();
+            transaction = session.beginTransaction();
+            session.createNativeQuery(CREATE_TABLE_SQL).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Ошибка при созданий", e);
         }
     }
 
     @Override
     public void dropUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createNativeQuery(drop).executeUpdate();
+            transaction = session.beginTransaction();
+            session.createNativeQuery(DROP_TABLE_SQL).executeUpdate();
             transaction.commit();
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Ошибка дропа", e);
         }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            try {
-                User user = new User();
-                user.setName ( name);
-                user.setLastName(lastName);
-                user.setAge(age);
-                session.save(user);
-                transaction.commit();
-            }catch (Exception e) {
-                transaction.rollback();
-                throw e;
-            }
+            transaction = session.beginTransaction();
+            User user = new User();
+            user.setName(name);
+            user.setLastName(lastName);
+            user.setAge(age);
+            session.save(user);
+            transaction.commit();
+
         } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Ошибка сохранении", e);
         }
     }
 
     @Override
     public void removeUserById(long id) {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
+            transaction = session.beginTransaction();
             User user = session.get(User.class, id);
             if (user != null) {
                 session.delete(user);
             }
             transaction.commit();
-        }catch (Exception e) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Ошибка удаления");
         }
     }
@@ -94,11 +99,15 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void cleanUsersTable() {
+        Transaction transaction = null;
         try (Session session = sessionFactory.openSession()) {
-            Transaction transaction = session.beginTransaction();
-            session.createNativeQuery(clean).executeUpdate();
+            transaction = session.beginTransaction();
+            session.createNativeQuery(CLEAN_TABLE_SQL).executeUpdate();
             transaction.commit();
-        }catch (Exception e) {
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
             throw new RuntimeException("Ошибка очистки", e);
         }
     }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -14,22 +14,27 @@ public class Util {
     private static final String PASSWORD = "123123";
     private static final String URL = "jdbc:mysql://localhost:3306/lesson";
 
-    public SessionFactory getSessionFactory() {
-        try {
-            Configuration cfg = new Configuration();
-            cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
-            cfg.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/lesson");
-            cfg.setProperty("hibernate.connection.username", "root");
-            cfg.setProperty("hibernate.connection.password", "123123");
-            cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
-            cfg.addAnnotatedClass(User.class);
-            ServiceRegistry srt = new StandardServiceRegistryBuilder()
-                    .applySettings(cfg.getProperties()).build();
+    public SessionFactory sessionFactory;
 
-            return cfg.buildSessionFactory(srt);
-        }catch (Exception e) {
-            throw new RuntimeException("ошибка H");
+    public SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration cfg = new Configuration();
+                cfg.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
+                cfg.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/lesson");
+                cfg.setProperty("hibernate.connection.username", "root");
+                cfg.setProperty("hibernate.connection.password", "123123");
+                cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL8Dialect");
+                cfg.addAnnotatedClass(User.class);
+                ServiceRegistry srt = new StandardServiceRegistryBuilder()
+                        .applySettings(cfg.getProperties()).build();
+
+                return cfg.buildSessionFactory(srt);
+            }catch (Exception e) {
+                throw new RuntimeException("ошибка H");
+            }
         }
+        return sessionFactory;
     }
 
     public static Connection getConnection() {
